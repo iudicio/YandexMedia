2587490df6f6269889e8616fc5db9e169f7b9c6d
user <user@email.com>
2025-10-26 15:43:57 +0300

    commit_3v324

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.google.android.material:material:1.12.0")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("androidx.datastore:datastore-preferences:1.1.1")
     implementation ("com.google.android.material:material:1.12.0")
--- app/src/main/java/com/example/yandexmedia/TrackViewHolder.kt
+++ app/src/main/java/com/example/yandexmedia/TrackViewHolder.kt
@@ -5,6 +5,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 class TrackViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val artworkImageView: ImageView = view.findViewById(R.id.artworkImageView)
@@ -17,8 +19,15 @@ class TrackViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         artistNameTextView.text = track.artistName
         trackTimeTextView.text = track.trackTime
 
+        // Используем Glide с плейсхолдером и скруглением
+        val requestOptions = RequestOptions()
+            .transform(RoundedCorners(16)) // скругление углов
+            .placeholder(R.drawable.ic_placeholder) // показывается, пока грузится
+            .error(R.drawable.ic_placeholder) // показывается при ошибке (например, нет интернета)
+
         Glide.with(itemView)
             .load(track.artworkUrl100)
+            .apply(requestOptions)
             .into(artworkImageView)
     }
 }
