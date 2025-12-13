060967e96cd21886ae74ca0c5fea505656f28c94
user <user@email.com>
2025-12-13 14:33:42 +0300

    commit_3v1

--- app/src/main/java/com/example/yandexmedia/Track.kt
+++ app/src/main/java/com/example/yandexmedia/Track.kt
@@ -1,8 +1,22 @@
 package com.example.yandexmedia
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Track(
+    val trackId: Long,
     val trackName: String,
     val artistName: String,
     val trackTime: String,
-    val artworkUrl100: String
-)
+    val artworkUrl100: String,
+    val collectionName: String? = null,
+    val releaseDate: String? = null,
+    val primaryGenreName: String? = null,
+    val country: String? = null,
+    val trackTimeMillis: Long? = null
+) : Parcelable {
+    fun getCoverArtwork(): String =
+        artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
+}
+
